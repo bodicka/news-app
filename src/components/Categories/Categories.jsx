eac13f555455693079c69737965811b8583ad2ba
bodicka <ms.s1506@mail.ru>
2024-11-22 19:24:40 +0300

    add fixed slider and banner scroll =-)

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,33 +1,34 @@
+import { forwardRef } from "react";
 import styles from "./styles.module.css";
 
-const Categories = ({
-  categories,
-  setSelectedCategories,
-  selectedCategories,
-}) => {
-  return (
-    <div className={styles.categories}>
-      <button
-        onClick={() => setSelectedCategories(null)}
-        className={!selectedCategories ? styles.active : styles.item}
-      >
-        All
-      </button>
-      {categories.map((categori) => {
-        return (
-          <button
-            onClick={() => setSelectedCategories(categori)}
-            className={
-              selectedCategories === categori ? styles.active : styles.item
-            }
-            key={categori}
-          >
-            {categori}
-          </button>
-        );
-      })}
-    </div>
-  );
-};
+const Categories = forwardRef(
+  ({ categories, setSelectedCategories, selectedCategories }, ref) => {
+    return (
+      <div ref={ref} className={styles.categories}>
+        <button
+          onClick={() => setSelectedCategories(null)}
+          className={!selectedCategories ? styles.active : styles.item}
+        >
+          All
+        </button>
+        {categories.map((categori) => {
+          return (
+            <button
+              onClick={() => setSelectedCategories(categori)}
+              className={
+                selectedCategories === categori ? styles.active : styles.item
+              }
+              key={categori}
+            >
+              {categori}
+            </button>
+          );
+        })}
+      </div>
+    );
+  }
+);
+
+Categories.displayName = "Categories";
 
 export default Categories;
--- src/components/NewsFilters/NewsFilters.jsx
+++ src/components/NewsFilters/NewsFilters.jsx
@@ -2,6 +2,7 @@ import { getCategories } from "../../api/apiNews";
 import { useFetch } from "../../helpers/hooks/useFetch";
 import Categories from "../Categories/Categories";
 import Search from "../Search/Search";
+import Slider from "../Slider/Slider";
 import styles from "./styles.module.css";
 
 const NewsFilters = ({ filter, changeFilter }) => {
@@ -10,13 +11,15 @@ const NewsFilters = ({ filter, changeFilter }) => {
   return (
     <div className={styles.filter}>
       {dataCategories ? (
-        <Categories
-          categories={dataCategories.categories}
-          selectedCategories={filter.category}
-          setSelectedCategories={(category) =>
-            changeFilter("category", category)
-          }
-        />
+        <Slider>
+          <Categories
+            categories={dataCategories.categories}
+            selectedCategories={filter.category}
+            setSelectedCategories={(category) =>
+              changeFilter("category", category)
+            }
+          />
+        </Slider>
       ) : null}
 
       <Search
