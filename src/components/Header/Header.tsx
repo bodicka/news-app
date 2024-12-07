2ee4c1d98deca3e2c350992aefe9d9ca1cfee7d6
bodicka <ms.s1506@mail.ru>
2024-12-07 20:53:28 +0300

    add context and change theme

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,21 @@
+import { themeIcon } from "../../assets/theme";
 import { formatDate } from "../../helpers/formatDate";
 import styles from "./styles.module.css";
+import { useTheme } from "../../context/ThemeContext";
 
 const Header = () => {
+  const { isDark, toogleTheme } = useTheme();
+
   return (
-    <header className={styles.header}>
-      <h1 className={styles.title}>NEWS REACTIFY</h1>
-      <p className={styles.date}>{formatDate(new Date())}</p>
+    <header
+      className={`${styles.header} ${isDark ? styles.dark : styles.light}`}
+    >
+      <div className={styles.info}>
+        <h1 className={styles.title}>NEWS REACTIFY</h1>
+        <p className={styles.date}>{formatDate(new Date())}</p>
+      </div>
+
+      <img src={isDark ? themeIcon.light : themeIcon.dark} width={30} alt="theme" onClick={toogleTheme} />
     </header>
   );
 };
